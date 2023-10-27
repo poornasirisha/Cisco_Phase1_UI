9673346a6c550b84a7120b5ceb22868bbcdcd0f0
Sudha Agarwal <sudha.agarwal84@gmail.com>
2023-10-27 15:57:24 +0530

    final code

--- Backend/server.js
+++ Backend/server.js
@@ -12,11 +12,15 @@ app.use(bodyParser.json()) // Parse JSON request bodies
 const products = [
 {id:1, name:'Product1',description:'Description1',category:'Mobile'},
   {id:2, name:'Product2',description:'Description2',category:'Mobile'},
-  {id:3, name:'Product3',description:'Description3',category:'Mobile'},
-  {id:4, name:'Product4',description:'Description4',category:'Mobile'},
-  {id:5, name:'Product5',description:'Description5',category:'Mobile'},
-  {id:6, name:'Product6',description:'Description6',category:'Mobile'},
-  {id:7, name:'Product7',description:'Description7',category:'Mobile'},
+  {id:3, name:'Product3',description:'Description3',category:'Tablet'},
+  {id:4, name:'Product4',description:'Description4',category:'Laptop'},
+  {id:5, name:'Product5',description:'Description5',category:'Laptop'},
+  {id:6, name:'Product6',description:'Description6',category:'Laptop'},
+  {id:7, name:'Product7',description:'Description7',category:'Tablet'},
+  {id:7, name:'Product8',description:'Description8',category:'Tablet'},
+  {id:7, name:'Product9',description:'Description9',category:'Tablet'},
+  {id:7, name:'Product10',description:'Description10',category:'Mobile'},
+  {id:7, name:'Product11',description:'Description11',category:'Mobile'},
 ];
 
 class User{
@@ -36,7 +40,16 @@ class User{
 const users = [new User('Sudha','Agarwal', 'sudha@gmail.com','123456')];
 
 app.get('/products',(req, res) =>{
-    res.status(200).json(products);
+    const category = req.query.category;
+    const filteredProducts = products.filter(product=> product.category === category);
+
+    if(filteredProducts.length === 0){
+        res.status(404).json({message: 'No product found for the selected category'});
+    }
+    else{
+        res.status(200).json(filteredProducts);
+    }
+   
 });
 
 app.post('/login', (req,res) => {
